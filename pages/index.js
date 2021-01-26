4a8658862ed554774990be86f860db30e5e7f546
Egemen Ceylan <egemen.ceylan1@gmail.com>
2021-01-26 22:27:33 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../components/Button';
+
+function HomePage() {
+    return (
+        <React.Fragment>
+            <div>Welcome to Next.js!</div>
+            <Button>Merhaba</Button>
+        </React.Fragment>
+    )
+}
+
+export default HomePage;
---
+++ stories/Icons.stories.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import * as Icons from '../components/icons';
+
+export default {
+    title: 'Example/Icons',
+};
+
+export const Icon = () => (
+    <div className="icons">
+        <Icons.ArrowBottom />
+        <Icons.Bookmark />
+        <Icons.BookmarkFill />
+        <Icons.Emoji />
+        <Icons.Explore />
+        <Icons.ExplorerFill />
+        <Icons.Gif />
+        <Icons.Home />
+        <Icons.HomeFill />
+        <Icons.Like />
+        <Icons.Lists />
+        <Icons.ListsFill />
+        <Icons.Media />
+        <Icons.Messages />
+        <Icons.MessagesFill />
+        <Icons.More />
+        <Icons.Notification />
+        <Icons.NotificationFill />
+        <Icons.Options />
+        <Icons.Profile />
+        <Icons.ProfileFill />
+        <Icons.Question />
+        <Icons.Reply />
+        <Icons.Retweet />
+        <Icons.Search />
+        <Icons.Share />
+        <Icons.TimelineProp />
+        <Icons.Twitter />
+    </div>
+)
+
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
